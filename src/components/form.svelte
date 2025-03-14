c8647b39d5269cd31fcdce4fafa2c5cb252475e2
Marsudi <marsudi.marsudi@nawatech.co>
2025-03-14 11:51:36 +0700

    first commit

---
+++ src/components/form.svelte
@@ -0,0 +1,14 @@
+<script>
+	let value = '';
+	function handleSubmit() {
+		console.log(value);
+	}
+</script>
+
+<div>
+	<form on:submit|preventDefault={handleSubmit}>
+		<input type="text" bind:value />
+		<button type="submit">Submit</button>
+	</form>
+	<p>{value}</p>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import Form from '../components/form.svelte';
+</script>
+
+<h1>ini form yang diambil via components</h1>
+<Form />
